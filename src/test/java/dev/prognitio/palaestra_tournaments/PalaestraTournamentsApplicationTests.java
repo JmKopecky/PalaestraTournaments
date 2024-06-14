a79aabc3f51bfe2c9ba96d23d10081dd7ea5851d
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-14 17:09:45 -0500

    Fixed wierd IDE issue

---
+++ src/test/java/dev/prognitio/palaestra_tournaments/PalaestraTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prognitio.palaestra_tournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalaestraTournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
