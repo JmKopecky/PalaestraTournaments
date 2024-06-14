c6516b04f47d2089aeb90208aa18179ffda88e11
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-14 11:55:02 -0500

    Began project

---
+++ src/main/java/dev/prognitio/palaestra_tournaments/PalaestraTournamentsApplication.java
@@ -0,0 +1,13 @@
+package dev.prognitio.palaestra_tournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalaestraTournamentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalaestraTournamentsApplication.class, args);
+    }
+
+}
