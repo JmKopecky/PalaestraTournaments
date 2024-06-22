795ef858ea0bfe622b40e68dc25bf486b58f6868
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-21 20:02:02 -0500

    Created interface for creating a tournament.

--- src/main/java/dev/prognitio/palaestra_tournaments/PalaestraTournamentsApplication.java
+++ src/main/java/dev/prognitio/palaestra_tournaments/PalaestraTournamentsApplication.java
@@ -1,5 +1,6 @@
 package dev.prognitio.palaestra_tournaments;
 
+import dev.prognitio.palaestra_tournaments.tournament.Tournament;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,10 @@ public class PalaestraTournamentsApplication {
 
     public static int PORT = 8080;
     public static HashMap<String, String> FACILITATOR_KEY = new HashMap<>();
+
+    public static Tournament tournament;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(PalaestraTournamentsApplication.class, args);
--- src/main/java/dev/prognitio/palaestra_tournaments/page_controller/ConfigureTournamentController.java
+++ src/main/java/dev/prognitio/palaestra_tournaments/page_controller/ConfigureTournamentController.java
@@ -1,33 +1,53 @@
 package dev.prognitio.palaestra_tournaments.page_controller;
 
+import dev.prognitio.palaestra_tournaments.Log;
 import dev.prognitio.palaestra_tournaments.PalaestraTournamentsApplication;
 import dev.prognitio.palaestra_tournaments.formdata.KeyFormData;
+import dev.prognitio.palaestra_tournaments.formdata.TournamentSetupFormData;
+import dev.prognitio.palaestra_tournaments.tournament.Competitor;
+import dev.prognitio.palaestra_tournaments.tournament.DefaultSettings;
+import dev.prognitio.palaestra_tournaments.tournament.Tournament;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 @Controller
 public class ConfigureTournamentController {
 
     @GetMapping("/tournamentsetup")
-    public String verifyFacilitator(Model model) {
+    public String setupTournament(Model model) {
 
-        String LANIP = "";
-
-        try {
-            InetAddress localhost = InetAddress.getLocalHost();
-            LANIP = localhost.getHostAddress();
-        } catch (UnknownHostException ex) {
-            LANIP = "Failed to retrieve the LAN IP. Is the server connected to a network?";
-        }
-
-        model.addAttribute("localip", LANIP + ":" + PalaestraTournamentsApplication.PORT);
-
-
+        model.addAttribute("tournamentSettingsFormData", new TournamentSetupFormData());
 
         return "tournament_setup";
+    }
+
+
+    @PostMapping("/tournamentsetup")
+    public String setupTournamentSubmit(Model model, @ModelAttribute("tournamentSettingsFormData") TournamentSetupFormData data) {
+        Log.log(Log.Type.INFO,
+                "Multiple Choice: " + data.isMultipleChoice() +
+                        " | Question Locking: " + data.isQuestionLocking() +
+                        " | Points Per Correct: " + data.getPointsPerCorrect() +
+                        " | Points Per Wrong: " + data.getPointsPerWrong() +
+                        " | Points Per Skipped: " + data.getPointsPerSkipped() +
+                        " | Point Scale: " + data.getPointScale(), this.getClass());
+
+        ArrayList<Competitor> competitorList = new ArrayList<>();
+
+        PalaestraTournamentsApplication.tournament = new Tournament(competitorList,
+                new DefaultSettings(
+                        data.isMultipleChoice(), data.isQuestionLocking(),
+                        data.getPointsPerCorrect(), data.getPointsPerWrong(),
+                        data.getPointsPerSkipped(), data.getPointScale()
+                ));
+
+        return "tournament_setup"; //"redirect:/URL"
     }
 }
